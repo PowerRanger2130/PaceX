09c18afc96e65dfcab301bcd6de9aad9cb91a8be
SGGHub <toth.gazember.adam@hotmail.hu>
2026-03-18 12:38:07 +0100

    a

--- webpages/scripts/deploy.sh
+++ webpages/scripts/deploy.sh
@@ -3,8 +3,9 @@ rm -rf /var/www/private
 rm -rf /var/www/public
 rm -rf /home/gls/db/server
 
-cp -r /home/gls/github/Documents/PaceX/private /var/www/
-cp -r /home/gls/github/Documents/PaceX/public /var/www/
-cp -r /home/gls/github/Documents/PaceX/server /home/gls/db/
+cp -r /home/gls/Documents/GitHub/PaceX/webpages/private/dist /var/www/private
+cp -r /home/gls/Documents/GitHub/PaceX/webpages/public/dist /var/www/public
+cp -r /home/gls/Documents/GitHub/PaceX/server /home/gls/db/
 
 systemctl restart apache2
+systemctl restart pacex-server
